2e490df55b944883710d1f1098b21a8c914d4b69
KasimovSarvar <sarvaruzb01@gmail.com>
2025-05-11 23:19:55 +0500

    changed

--- lead/views.py
+++ lead/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import OutcomeSerializer
+from .models import Outcome
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def outcome_view(request):
+    if request.user.role != 3:
+        return Response({'message': 'Permission denied'}, status=status.HTTP_403_FORBIDDEN)
+
+    if request.method == 'GET':
+        outcomes = Outcome.objects.all()
+        serializer = OutcomeSerializer(outcomes, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = OutcomeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
