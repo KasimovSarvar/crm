15793ffddd07b4fc82d68a7bc09d12cac079701a
JONY-99 <s.kaimjonov99@gmail.com>
2025-05-12 16:42:21 +0500

    test

--- lead/views.py
+++ lead/views.py
@@ -12,86 +12,93 @@ from rest_framework import status
 
 
 @api_view(['GET'])
-def lead_list_view(request):
-    lead = Lead.objects.all()
-    serializer = LeadSerializer(lead, many=True)
-    return Response({'message':'success'},serializer.data, status=status.HTTP_200_OK)
+def lead_list(request):
+    if not request.user.is_authenticated:
+        return Response({"error": "Not authenticated"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.user.role == 4:
+        leads = Lead.objects.filter(admin=request.user)
+    else:
+        leads = Lead.objects.all()
+    
+    serializer = LeadSerializer(leads, many=True)
+    return Response(serializer.data)
 
-@api_view(['GET'])
-def my_lead_list_view(request):
-    user = request.user
-    if user.is_authenticate and user.role == 4:
-        lead = Lead.objects.filter(admin=user)
-        serializer = LeadSerializer(lead, many=True)
-        return Response({'message':'success'},serializer.data)
-    return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
 
 @api_view(['PUT'])
 def lead_update_view(request, lead_id):
-    user = request.user
-    try:
-        lead = Lead.objects.get(id=lead_id)
-    except Lead.DoesNotExist:
-        return Response({'detail': 'Lead not found'}, status=status.HTTP_404_NOT_FOUND)
+    if not request.user.is_authenticated:
+        return Response({"error": "Not authenticated"}, status=status.HTTP_400_BAD_REQUEST)
     
-    if user.role in ['Admin', 'SuperUser'] and lead.admin == user:
+    user = request.user
+    
+    lead = Lead.objects.filter(id=lead_id).first()
+    if not lead:
+        return Response({'message': 'Lead not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.user.role == 4:
         serializer = LeadSerializer(lead, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response({'message':'success! update this lead'},serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
     
-    return Response({'detail': 'You are not authorized to update this lead'}, status=status.HTTP_403_FORBIDDEN)
 
  
 
 @api_view(['POST'])
-def create_student_view(request, lead_id):
-    data = request.data
-    user = request.user
-    
-    lead = Lead.objects.get(id=data['lead'])
-    
-    if not user.is_authenticated and user.role == 4:
-        return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
-    
-    try:
-        lead = Lead.objects.get(id=lead_id)
-    except Lead.DoesNotExist:
-        return Response({'detail': 'Lead not found'}, status=status.HTTP_404_NOT_FOUND)
-    
-    if not lead.admin == user:
-        return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
-    
-    serializer = StudentSerializer(data=data)
+def create_student(request):
+    if not request.user.is_authenticated:
+        return Response({"error": "Not authenticated"}, status=status.HTTP_400_BAD_REQUEST)
+
+    lead_id = request.data.get("lead")
+
+    if not lead_id:
+        return Response({'message': 'Lead id not found'}, status=400)
+
+    lead = Lead.objects.filter(id=lead_id).first()
+    if not lead:
+        return Response({'message': 'Lead not found'}, status=404)
+
+    if request.user.role == 4 and lead.admin != request.user:
+        return Response({'message': 'this lead not for you'}, status=403)
+
+    serializer = StudentSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return HttpResponse(serializer.data, status=status.HTTP_201_CREATED)
-    return Response({'message':'error in creating student'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
 
 @api_view(['GET'])
 def my_students_list_view(request):
-    user = request.user
-    if user.is_authenticate and user.role == 4:
-        student = Student.objects.filter(admin=user)
+    if not request.user.is_authenticated:
+        return Response({"error": "Not authenticated"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.user.role == 4:
+        student = Student.objects.filter(admin=request.user)
         serializer = StudentSerializer(student, many=True)
         return Response({'message':'success'},serializer.data)
-    return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
+    return Response({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
 
 @api_view(['PATCH'])
 def student_update_view(request, pk):
+    if not request.user.is_authenticated:
+        return Response({"error": "Not authenticated"}, status=status.HTTP_400_BAD_REQUEST)
+    
     data = request.data
     user = request.user
-    try:
-        student = Student.objects.get(id=pk)
-    except Student.DoesNotExist:
-        return Response({'detail': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    student = Student.objects.filter(id=pk).first()
+    if not student:
+        return Response({'message': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
     
-    if not user.is_authenticated and user.role == 4:
-        return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
+    if user.role != 4:
+        return Response({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
     
     if not student.admin == user:
-        return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
+        return Response({'message':'You can not update this student'}, status=status.HTTP_403_FORBIDDEN)
     
     serializer = StudentSerializer(student, data=data, partial=True)
     if serializer.is_valid():
@@ -99,109 +106,19 @@ def student_update_view(request, pk):
         return Response({'message':'success! update this student'},serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
-def outcome_view(request):
-    if request.user.role != 3:
-        return Response({'message': 'Permission denied'}, status=status.HTTP_403_FORBIDDEN)
-
-    if request.method == 'GET':
-        outcomes = Outcome.objects.all()
-        serializer = OutcomeSerializer(outcomes, many=True)
-        return Response(serializer.data)
-
-    if request.method == 'POST':
-        serializer = OutcomeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 @api_view(['GET'])
-def lead_list_view(request):
-    lead = Lead.objects.all()
-    serializer = LeadSerializer(lead, many=True)
-    return Response({'message':'success'},serializer.data, status=status.HTTP_200_OK)
+def student_detail(request, id):
+    if not request.user.is_authenticated:
+        return Response({"error": "Not authenticated"}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def my_lead_list_view(request):
-    user = request.user
-    if user.is_authenticate and user.role == 4:
-        lead = Lead.objects.filter(admin=user)
-        serializer = LeadSerializer(lead, many=True)
-        return Response({'message':'success'},serializer.data)
-    return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
+    student = Student.objects.filter(id=id).first()
+    if not student:
+        return Response({'message': 'Student not found'}, status=404)
 
-@api_view(['PUT'])
-def lead_update_view(request, lead_id):
-    user = request.user
-    try:
-        lead = Lead.objects.get(id=lead_id)
-    except Lead.DoesNotExist:
-        return Response({'detail': 'Lead not found'}, status=status.HTTP_404_NOT_FOUND)
-    
-    if user.role in ['Admin', 'SuperUser'] and lead.admin == user:
-        serializer = LeadSerializer(lead, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'message':'success! update this lead'},serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    return Response({'detail': 'You are not authorized to update this lead'}, status=status.HTTP_403_FORBIDDEN)
+    if request.user.role == 4 and student.lead.admin != request.user:
+        return Response({'message': 'this student not for you'}, status=403)
 
- 
-
-@api_view(['POST'])
-def create_student_view(request, lead_id):
-    data = request.data
-    user = request.user
-    
-    lead = Lead.objects.get(id=data['lead'])
-    
-    if not user.is_authenticated and user.role == 4:
-        return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
-    
-    try:
-        lead = Lead.objects.get(id=lead_id)
-    except Lead.DoesNotExist:
-        return Response({'detail': 'Lead not found'}, status=status.HTTP_404_NOT_FOUND)
-    
-    if not lead.admin == user:
-        return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
-    
-    serializer = StudentSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return HttpResponse(serializer.data, status=status.HTTP_201_CREATED)
-    return Response({'message':'error in creating student'}, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-def my_students_list_view(request):
-    user = request.user
-    if user.is_authenticate and user.role == 4:
-        student = Student.objects.filter(admin=user)
-        serializer = StudentSerializer(student, many=True)
-        return Response({'message':'success'},serializer.data)
-    return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
-
-@api_view(['PATCH'])
-def student_update_view(request, pk):
-    data = request.data
-    user = request.user
-    try:
-        student = Student.objects.get(id=pk)
-    except Student.DoesNotExist:
-        return Response({'detail': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
-    
-    if not user.is_authenticated and user.role == 4:
-        return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
-    
-    if not student.admin == user:
-        return HttpResponse({'message':'You are not Admin'}, status=status.HTTP_403_FORBIDDEN)
-    
-    serializer = StudentSerializer(student, data=data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({'message':'success! update this student'},serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer = StudentSerializer(student)
+    return Response(serializer.data)
 
  
